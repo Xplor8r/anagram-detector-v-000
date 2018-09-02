591cc4161185f91fdbe185252de0dd5ed721bb86
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-02 00:48:55 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,7 +5,8 @@ class Anagram
     @word = word
   end
   def match(words)
-    @wordschars.to_a.permutation.map(&:join).uniq.each do |anagram|
+    @wordschars.to_a.permutation.map(&:join).uniq
+    
     end
   end
 end
